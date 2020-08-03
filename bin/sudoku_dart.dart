3cfb3594739b10e3fcd46ad779e4e4ebefafba0a
Grant Montgomery <grantm@rusondia.net>
2020-08-03 13:52:01 -0500

    reworked sudoku_dart.dart to create a SudokuProblem and solve it

--- bin/sudoku_dart.dart
+++ bin/sudoku_dart.dart
@@ -1,10 +1,47 @@
 import 'dart:core';
 import 'domains/sudoku/SudokuProblem.dart';
 import 'domains/sudoku/SudokuState.dart';
+import 'framework/problem/SolvingAssistant.dart';
+
+SudokuProblem problem;
+SolvingAssistant assistant;
+int board_size;
+int cell_size;
+
+void doMove(int num, int row, int col) {
+  if (!problem.success()) {
+    var move = 'Place ' +
+        num.toString() +
+        ' at ' +
+        row.toString() +
+        ' ' +
+        col.toString();
+    assistant.tryMove(move);
+  }
+}
+
+void solveGame() {
+  for (var i = 0; i < board_size; i++) {
+    for (var j = 0; j < board_size; j++) {
+      for (var k = 1; k <= board_size; k++) {
+        doMove(k, i, j);
+        if (problem.isCorrect(i, j)) {
+          break;
+        }
+      }
+    }
+  }
+}
 
 void main(List<String> arguments) {
   // var sudoku = Sudoku.withMoreHints(0);
-  var problem = SudokuProblem();
-  SudokuState initial_state = problem.getInitialState();
-  print(problem.getStateAsString(initial_state));
+  problem = SudokuProblem();
+  assistant = SolvingAssistant(problem);
+  cell_size = problem.cell_size;
+  board_size = problem.board_size;
+  SudokuState current_state = problem.getCurrentState();
+  print(problem.getStateAsString(current_state));
+  solveGame();
+  current_state = problem.getCurrentState();
+  print(problem.getStateAsString(current_state));
 }
