9f0826718c00b1e59d790ef14274de8dfd326588
Grant Montgomery <grantm@rusondia.net>
2020-11-16 23:56:54 -0600

    added cli parsing for initial hints, and a -h flag to learn how to use the program.

--- bin/sudoku_dart.dart
+++ bin/sudoku_dart.dart
@@ -1,4 +1,8 @@
+//public packages
 import 'dart:core';
+import 'package:args/args.dart';
+
+//My stuff
 import 'domains/sudoku/SudokuProblem.dart';
 import 'domains/sudoku/SudokuState.dart';
 import 'framework/problem/SolvingAssistant.dart';
@@ -7,14 +11,27 @@ SudokuProblem problem;
 SolvingAssistant assistant;
 int board_size;
 int cell_size;
+ArgResults argResults;
+String help =
+    'Usage: sudoku [OPTION]...\nA simple dart program to generate and display a sudoku game.\n\n  -h, --help          Display this message\n  -i, --initialHints  number of givens on starting board, defaulting to 30';
 
 void main(List<String> arguments) {
-  // var sudoku = Sudoku.withMoreHints(0);
-  problem = SudokuProblem.withMoreHints(13);
+  final parser = ArgParser()
+    ..addOption('initialHints',
+        abbr: 'i', help: 'number of givens on board', defaultsTo: '30')
+    ..addFlag('help', abbr: 'h');
 
-  SudokuState current_state = problem.getCurrentState();
-  print('Initial Board:');
-  print(problem.getStateAsString(current_state));
-  print('Solution:');
-  print(problem.getStateAsString(problem.getFinalState()));
+  argResults = parser.parse(arguments);
+
+  if (argResults['help']) {
+    print(help);
+  } else {
+    var hints = int.parse(argResults['initialHints']) - 17;
+    problem = SudokuProblem.withMoreHints(hints);
+    SudokuState current_state = problem.getCurrentState();
+    print('Initial Board:');
+    print(problem.getStateAsString(current_state));
+    print('Solution:');
+    print(problem.getStateAsString(problem.getFinalState()));
+  }
 }
