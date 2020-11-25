64d3a5bf148b586a9872e5a7e2f24df9ff2f89dc
Grant Montgomery <grantm@rusondia.net>
2020-11-25 01:32:09 -0600

    update to use dart compile

--- test-native.sh
+++ test-native.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir build
-dart2native bin/sudoku_dart.dart -o build/sudoku
+dart compile exe bin/sudoku_dart.dart -o build/sudoku
 for f in {1..50}
 do
     time build/sudoku >> /dev/null
