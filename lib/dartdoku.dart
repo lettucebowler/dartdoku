91a05437b7e307c4dda2ca49587d491ca1015a4a
Grant Montgomery <grantm@rusondia.net>
2021-03-26 14:31:01 -0500

    added bin for local testing

--- lib/dartdoku.dart
+++ lib/dartdoku.dart
@@ -1,7 +1,9 @@
 //public packages
 import 'dart:core';
 import 'package:args/args.dart';
-import 'package:dartdoku/dartdoku.dart';
+
+//My stuff
+import 'domains/sudoku/SudokuProblem.dart';
 
 String help =
     'Usage: sudoku [OPTION]...\nA simple dart program to generate and display a sudoku game.\n\n  -h, --help          Display this message\n  -i, --initialHints  number of givens on starting board, defaulting to 30';
@@ -21,11 +23,24 @@ void main(List<String> arguments) {
     var hints = int.parse(argResults['initialHints']) - 17;
     var count = int.parse(argResults['count']);
     var problems = getProblems(hints, count);
-    for (var problem in problems) {
+    for (SudokuProblem problem in problems) {
       print('Initial Board:');
       print(problem.getStateAsString(problem.getCurrentState()));
       print('Solution:');
       print(problem.getStateAsString(problem.getFinalState()));
     }
   }
+}
+
+List<SudokuProblem> getProblems(int hints, int count) {
+  var problems = <SudokuProblem>[];
+  for (var i = 0; i < count; i++) {
+    var problem = SudokuProblem.withMoreHints(hints);
+    problems.add(problem);
+  }
+  return problems;
+}
+
+SudokuProblem getProblem(int hints) {
+  return SudokuProblem.withMoreHints(hints);
 }
